39aed22f3441cd1463222dd28b49388d8c25a47c
sousagomide <sousagomide@gmail.com>
2025-01-13 11:41:13 -0300

    update

---
+++ aula 01/funcoes/com_funcao.js
@@ -0,0 +1,9 @@
+function imprimirData(d, m, a){
+    console.log(`Dia: ${d}`)
+    console.log(`Mês: ${m}`)
+    console.log(`Ano: ${a}`)
+    console.log(`${d}/${m}/${a}`)
+}
+
+imprimirData(24, 'Janeiro', 2025)
+imprimirData(19, 'Dezembro', 2025)
---
+++ aula 01/funcoes/sem_funcao.js
@@ -0,0 +1,18 @@
+const d1 = 24
+const m1 = 'Janeiro'
+const a1 = 2025
+
+console.log(`Dia: ${d1}`)
+console.log(`Mês: ${m1}`)
+console.log(`Ano: ${a1}`)
+console.log(`${d1}/${m1}/${a1}`)
+
+const d2 = 19
+const m2 = 'Dezembro'
+const a2 = 2025
+
+console.log(`Dia: ${d2}`)
+console.log(`Mês: ${m2}`)
+console.log(`Ano: ${a2}`)
+console.log(`${d2}/${m2}/${a2}`)
+
