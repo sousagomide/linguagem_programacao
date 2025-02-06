9ec65003a9d79725fcd8f0fa2e5685a4d19fdfe2
sousagomide <sousagomide@gmail.com>
2025-02-06 18:20:30 -0300

    Update funcoes_array.js

--- aula 01/arrays/funcoes_array.js
+++ aula 01/arrays/funcoes_array.js
@@ -21,29 +21,26 @@ const v3 = a3.indexOf(2)
 const v4 = a3.join()
 // 1,2,3,0,0,6
 
+const v5 = a3.lastIndexOf(6)
+// 5
 
+a3.pop()
+// [1, 2, 3, 0, 0]
 
+a3.push(13)
+// [1, 2, 3, 0, 0, 13]
 
+const v6 = a3.reverse()
+// [13, 0, 0, 3, 2, 1]
 
-console.log(v4)
+const v7 = a3.slice(1, 3)
+// [0, 0]
 
+const v8 = a3.sort()
+// [0, 0, 1, 13, 2, 3]
 
+const v9 = a3.toSpliced(1, 0, 69)
+// [0, 69, 0, 1, 13, 2, 3]
 
-
-// concat - OK
-// fill - OK
-// filter - OK
-// find - OK
-// forEach - OK
-// includes - OK
-// indexOf - OK
-// join - OK
-// lastIndexOf
-// map - OK
-// pop
-// push
-// reduce - OK
-// reverse
-// slice
-// sort
-// splice
+const v10 = a3.toSpliced(4, 2, 79)
+// [0, 0, 1, 13, 79]
